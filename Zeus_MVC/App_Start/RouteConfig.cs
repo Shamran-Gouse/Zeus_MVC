b9cb7c6478573d3999c895e653d448aa820fde80
Shamran Gouse <shamrangouse.sg@gmail.com>
2019-05-04 01:27:25 +0530

    Admin Area Added

--- Zeus_MVC/App_Start/RouteConfig.cs
+++ Zeus_MVC/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace Zeus_MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Zeus_MVC.Controllers" }
             );
         }
     }
