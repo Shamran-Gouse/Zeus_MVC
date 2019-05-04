18489cb62283476b18db7c750c27619887ef6ec9
Shamran Gouse <shamrangouse.sg@gmail.com>
2019-05-04 20:38:36 +0530

    Product Section added in index

---
+++ Zeus_MVC/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zeus_MVC.Models;
+
+namespace Zeus_MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ProductController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ChildActionOnly]
+        public ActionResult ProductSection()
+        {
+            try
+            {
+                var ProductsList = _context.Products.OrderByDescending(p => p.Id).Take(8).ToList();
+
+                return PartialView(ProductsList);
+            }
+            catch
+            {
+                return PartialView();
+            }
+        }
+    }
+}
