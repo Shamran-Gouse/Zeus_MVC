e169e33031a1e6c3402f3f0ee53c69f228cbd338
Shamran Gouse <shamrangouse.sg@gmail.com>
2019-05-06 20:10:19 +0530

    Order Index and Detail Added

--- Zeus_MVC/Controllers/OrderController.cs
+++ Zeus_MVC/Controllers/OrderController.cs
@@ -1,23 +1,82 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using Zeus_MVC.Models;
+using Zeus_MVC.ViewModels;
 
 namespace Zeus_MVC.Controllers
 {
     public class OrderController : Controller
     {
-        // GET: Order
-        public ActionResult Index()
+        ApplicationDbContext context;
+
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
         {
-            return View();
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public OrderController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        public OrderController(ApplicationUserManager userManager) : base()
+        {
+            UserManager = userManager;
+        }
+        
+
+        // GET: Order
+        public async Task<ActionResult> Index()
+        {
+            var user = await UserManager.FindByNameAsync(User.Identity.Name);
+
+            var userOrders = context.Orders.Where(o => o.UserId == user.Id).ToList();
+
+            if (userOrders == null)
+                return RedirectToAction("Error404", "Home");
+
+            return View(userOrders);
         }
 
         // GET: Order/Success
         public ActionResult Success()
         {
             return View();
+        }
+
+        // GET: Order/Success
+        public async Task<ActionResult> Details(int id)
+        {
+            var user = await UserManager.FindByNameAsync(User.Identity.Name);
+
+            var order = context.Orders.SingleOrDefault(o => o.Id == id);
+
+            var orderdetails = context.OrderDetails.Include(p => p.Product).Where(o => o.OrderId == id).ToList();
+
+
+            var viewModel = new OrderDetailViewModel()
+            {
+                Client = user,
+                Order = order,
+                OrderDetails = orderdetails
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ Zeus_MVC/ViewModels/OrderDetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Zeus_MVC.Models;
+
+namespace Zeus_MVC.ViewModels
+{
+    public class OrderDetailViewModel
+    {
+        public Order Order { get; set; }
+
+        public ApplicationUser Client { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
